997d4494df22111d706ad79a8bf7db05360f2c67
Just_Kvin <144643334+Kairbros@users.noreply.github.com>
2026-07-20 23:54:15 -0500

    a

--- primera_prueba/app/Models/Publicacion.php
+++ primera_prueba/app/Models/Publicacion.php
@@ -15,5 +15,5 @@ class Publicacion extends Model
     protected $table = 'publicaciones';
 
     // Columnas que se pueden llenar con create()/update() pasando un array.
-    protected $fillable = ['titssssulo', 'descripcion', 'ruta_archivo'];
+    protected $fillable = ['titulos', 'descripcion', 'ruta_archivo'];
 }
